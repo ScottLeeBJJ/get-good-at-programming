2f7818dceaaf50fd0e02fdc849929df6220d36c5
Scott Lee <scottlee5455@outlook.com>
2024-10-13 04:26:46 -0500

    setting up problem 1

---
+++ jspractice.js
@@ -0,0 +1,8 @@
+//Prep for interviews:
+//Round 1: Data Structures and Algorithms
+
+//Problem 1 - Arrays and Strungs:
+//How would you find the first non-repeating character in a given string?
+//What is the time complexity of your solution?
+
+const noRepeat = [0,0,1,1,2,3,3,4,4,5,5];
