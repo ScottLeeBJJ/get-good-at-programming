d8267ac8c42bf7e88cb5b61abf0acd247d389133
Scott Lee <56131751+ScottLeeBJJ@users.noreply.github.com>
2024-12-25 19:32:36 +0000

    test on tablet

--- main.cpp
+++ main.cpp
@@ -9,12 +9,12 @@
 
 // Input and Output
 
-using namespace std; // Allows you to avoid prefixing standard library functions and objects with std::
+// using namespace std; // Allows you to avoid prefixing standard library functions and objects with std::
 
-int main() { // Only one per program. Defines the main function, the entry point of your program. Every C++ program starts with execution from the 'main' function. The return type is 'int', and the function returns '0' at the end to indicate successful execution.
-    int number; 
-    cout << "Enter a number: ";
-    cin >> number;
-    cout << "You entered: " << number;
-    return 0;
-}
+// int main() { // Only one per program. Defines the main function, the entry point of your program. Every C++ program starts with execution from the 'main' function. The return type is 'int', and the function returns '0' at the end to indicate successful execution.
+//     int number; 
+//     cout << "Enter a number: ";
+//     cin >> number;
+//     cout << "You entered: " << number;
+//     return 0;
+// }
