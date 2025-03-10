1c032688827239cb826dea9155235ff9473644f3
Scott Lee <56131751+ScottLeeBJJ@users.noreply.github.com>
2025-03-10 10:37:52 +0000

    starting c++ practice

---
+++ practice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main
+(
+    char myCharacter;
+    myCharacter = 'y';
+
+    int myInt;
+    myInt = 13;
+
+    cout << myCharacter << endl;
+
+    stystem("pause");
+)
